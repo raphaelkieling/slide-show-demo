a426fb1707d7c5a85e5c0daf43856e3e45702eea
Raphael Kieling <rapha_kieling@hotmail.com>
2018-07-09 18:41:08 -0300

    Images e estilo de slider

--- src/slider.js
+++ src/slider.js
@@ -1,24 +1,75 @@
-function SpSlider(context, { TIME = 1000, transition = 'fadeIn', color = "black" }) {
+function SpSlider(context, { TIME = 1000, transition = 'fadeIn', color = "black", gesture = true }) {
     this.context = document.querySelector(context);
     this.slide_index = 1;
     this.step_timer = null;
+    this.inGesture = false;
     this.showSlides(1);
 
-    this.config = {
+    this.config = this.configHTML() || {
         TIME,
         transition,
-        color
+        color,
+        gesture
     }
 
     this.setTransition(this.config.transition);
     this.setColorBackground()
     this.play();
+    this.gesture();
 
     return this;
 }
 
+SpSlider.prototype.configHTML = function () {
+    let TIME = this.context.getAttribute('slider-time');
+    let transition = this.context.getAttribute('slider-transition');
+    let color = this.context.getAttribute('slider-color');
+    let gesture = this.context.getAttribute('slider-gesture');
+
+    if (TIME || transition || color || gesture) {
+        return {
+            TIME,
+            transition,
+            color,
+            gesture
+        }
+    } else {
+        return false;
+    }
+
+}
+
+SpSlider.prototype.gesture = function (n = 1) {
+    if (this.config.gesture == false) return;
+
+    const mc = new Hammer(this.context);
+    mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+    mc.on("panleft panright", (ev) => {
+        if (this.inGesture == true) return;
+
+        this.inGesture = true;
+        switch (ev.type) {
+            case "panleft":
+                this.prevSlides();
+                break;
+
+            case "panright":
+                this.plusSlides();
+                break;
+        }
+    });
+
+    mc.on("panend pancancel", (ev) => {
+        this.inGesture = false;
+    })
+}
+
 SpSlider.prototype.plusSlides = function (n = 1) {
     this.showSlides(this.slide_index += n);
+}
+
+SpSlider.prototype.prevSlides = function (n = 1) {
+    this.showSlides(this.slide_index -= n);
 }
 
 SpSlider.prototype.currentSlide = function (n) {
@@ -31,9 +82,9 @@ SpSlider.prototype.showSlides = function (n) {
     if (n > slides.length) { this.slide_index = 1 }
     if (n < 1) { this.slide_index = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove('active')
     }
-    slides[this.slide_index - 1].style.display = "block";
+    slides[this.slide_index - 1].classList.add('active')
 }
 
 SpSlider.prototype.getSlides = function () {
